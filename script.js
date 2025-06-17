7c774f1b996331697501e3e35560d890aa0e5fd1
Bill B <wobean@yahoo.com>
2025-06-17 19:31:27 -0400

    looping id assignments

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
+const ability = ["strScore", "dexScore", "conScore", "intScore", "wisScore", "chaScore",];
+
+const slot = [];
+for (let i = 1; i <= 20; i++) {
+    slot.push("slot-" + i);
+}
+
 function getStoredValues() {
     // Set player level
     // document.getElementById("playerLevel").value = localStorage.getItem("playerLevel");
 
     // Set ability scores
-    document.getElementById("strScore").value = localStorage.getItem("strScore");
-    document.getElementById("dexScore").value = localStorage.getItem("dexScore");
-    document.getElementById("conScore").value = localStorage.getItem("conScore");
-    document.getElementById("intScore").value = localStorage.getItem("intScore");
-    document.getElementById("wisScore").value = localStorage.getItem("wisScore");
-    document.getElementById("chaScore").value = localStorage.getItem("chaScore");
+    ability.forEach(abilityItem);
+    function abilityItem(item) {
+        document.getElementById(item).value = localStorage.getItem(item);
+    }
 
     // Set inventory slots
-    document.getElementById("slot-1").value = localStorage.getItem("slot-1");
-    document.getElementById("slot-2").value = localStorage.getItem("slot-2");
-    document.getElementById("slot-3").value = localStorage.getItem("slot-3");
-    document.getElementById("slot-4").value = localStorage.getItem("slot-4");
-    document.getElementById("slot-5").value = localStorage.getItem("slot-5");
-    document.getElementById("slot-6").value = localStorage.getItem("slot-6");
-    document.getElementById("slot-7").value = localStorage.getItem("slot-7");
-    document.getElementById("slot-8").value = localStorage.getItem("slot-8");
-    document.getElementById("slot-9").value = localStorage.getItem("slot-9");
-    document.getElementById("slot-10").value = localStorage.getItem("slot-10");
-    document.getElementById("slot-11").value = localStorage.getItem("slot-11");
-    document.getElementById("slot-12").value = localStorage.getItem("slot-12");
-    document.getElementById("slot-13").value = localStorage.getItem("slot-13");
-    document.getElementById("slot-14").value = localStorage.getItem("slot-14");
-    document.getElementById("slot-15").value = localStorage.getItem("slot-15");
-    document.getElementById("slot-16").value = localStorage.getItem("slot-16");
-    document.getElementById("slot-17").value = localStorage.getItem("slot-17");
-    document.getElementById("slot-18").value = localStorage.getItem("slot-18");
-    document.getElementById("slot-19").value = localStorage.getItem("slot-19");
-    document.getElementById("slot-20").value = localStorage.getItem("slot-20");
+    slot.forEach(slotItem);
+    function slotItem(item) {
+        document.getElementById(item).value = localStorage.getItem(item);
+    }
 
     // Set AC
     // document.getElementById("armorClass").value = localStorage.getItem("armorClass");
@@ -45,34 +34,16 @@ function setLocalStorage() {
     // localStorage.setItem("playerLevel", document.getElementById("playerLevel").value);
 
     // Save ability scores
-    localStorage.setItem("strScore", document.getElementById("strScore").value);
-    localStorage.setItem("dexScore", document.getElementById("dexScore").value);
-    localStorage.setItem("conScore", document.getElementById("conScore").value);
-    localStorage.setItem("intScore", document.getElementById("intScore").value);
-    localStorage.setItem("wisScore", document.getElementById("wisScore").value);
-    localStorage.setItem("chaScore", document.getElementById("chaScore").value);
+    ability.forEach(abilityItem);
+    function abilityItem(item) {
+        localStorage.setItem(item, document.getElementById(item).value);
+    }
 
     // Save inventory slots
-    localStorage.setItem("slot-1", document.getElementById("slot-1").value);
-    localStorage.setItem("slot-2", document.getElementById("slot-2").value);
-    localStorage.setItem("slot-3", document.getElementById("slot-3").value);
-    localStorage.setItem("slot-4", document.getElementById("slot-4").value);
-    localStorage.setItem("slot-5", document.getElementById("slot-5").value);
-    localStorage.setItem("slot-6", document.getElementById("slot-6").value);
-    localStorage.setItem("slot-7", document.getElementById("slot-7").value);
-    localStorage.setItem("slot-8", document.getElementById("slot-8").value);
-    localStorage.setItem("slot-9", document.getElementById("slot-9").value);
-    localStorage.setItem("slot-10", document.getElementById("slot-10").value);
-    localStorage.setItem("slot-11", document.getElementById("slot-11").value);
-    localStorage.setItem("slot-12", document.getElementById("slot-12").value);
-    localStorage.setItem("slot-13", document.getElementById("slot-13").value);
-    localStorage.setItem("slot-14", document.getElementById("slot-14").value);
-    localStorage.setItem("slot-15", document.getElementById("slot-15").value);
-    localStorage.setItem("slot-16", document.getElementById("slot-16").value);
-    localStorage.setItem("slot-17", document.getElementById("slot-17").value);
-    localStorage.setItem("slot-18", document.getElementById("slot-18").value);
-    localStorage.setItem("slot-19", document.getElementById("slot-19").value);
-    localStorage.setItem("slot-20", document.getElementById("slot-20").value);
+    slot.forEach(slotItem);
+    function slotItem(item) {
+        localStorage.setItem(item, document.getElementById(item).value);
+    }
 
     // Save AC
     // localStorage.setItem("armorClass", document.getElementById("armorClass").value);
